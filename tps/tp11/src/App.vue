c9e0ce830ab3a32841d800e557025c8ca9fe7a47
Grimille <mickael.dhainaut@gmail.com>
2023-07-18 20:41:32 +0200

    🚀 TP-11

---
+++ tps/tp11/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <Alert>
+    <template v-slot:title>Super alerte !</template>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+  </Alert>
+</template>
+
+<script setup>
+import Alert from '@/components/Alert.vue';
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  font-family: 'ABeeZee', sans-serif;
+  font-size: 16px;
+}
+
+</style>
---
+++ tps/tp11/src/components/Alert.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="alert">
+    <h3><slot name="title">Message important !</slot></h3>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.alert {
+  padding: 2em;
+  background-color: rgb(175, 65, 84, 0.8);
+  border: 1px solid rgb(175, 65, 84);
+  color: white;
+  margin: 2em auto;
+  max-width: 500px;
+  border-radius: 4px;
+}
+
+h3 {
+  margin-bottom: 1em;
+}
+</style>
